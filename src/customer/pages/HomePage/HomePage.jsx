47e10d92176d85e4206d408710a6250ef34cf07b
Lin-Thet-Zaw <ammoaalinthetzaw@gmail.com>
2025-01-20 18:38:56 +0630

    add meta tag and key words

--- src/customer/pages/HomePage/HomePage.jsx
+++ src/customer/pages/HomePage/HomePage.jsx
@@ -1,18 +1,25 @@
-import React from 'react'
-import MainCarousel from '../../componenets/HomeCarousel/MainCarousel'
-import HomeSectionCarousel from '../../componenets/HomeSectionCarousel/HomeSectionCarousel'
+import React from "react";
+import MainCarousel from "../../componenets/HomeCarousel/MainCarousel";
+import HomeSectionCarousel from "../../componenets/HomeSectionCarousel/HomeSectionCarousel";
 
 function HomePage() {
   return (
     <div>
-        <MainCarousel />
-        <div className='space-y-10 py-20 flex flex-col'>
-            <HomeSectionCarousel sectionName={"Man Shoes"}/>
-            <HomeSectionCarousel sectionName={"Man Wear"}/>
-            <HomeSectionCarousel sectionName={"Man Shirt"}/>
-        </div>
+      <Helmet>
+        <title>Home - Ninja Shop</title>
+        <meta
+          name="description"
+          content="Welcome to the homepage of our app."
+        />
+      </Helmet>
+      <MainCarousel />
+      <div className="space-y-10 py-20 flex flex-col">
+        <HomeSectionCarousel sectionName={"Man Shoes"} />
+        <HomeSectionCarousel sectionName={"Man Wear"} />
+        <HomeSectionCarousel sectionName={"Man Shirt"} />
+      </div>
     </div>
-  )
+  );
 }
 
-export default HomePage
+export default HomePage;
