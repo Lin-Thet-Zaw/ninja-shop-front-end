7e7b451f982149c51c2d5d348a7372ed73df71e7
Lin-Thet-Zaw <ammoaalinthetzaw@gmail.com>
2025-01-21 17:16:47 +0630

    hide naver cart if user not auth

--- src/customer/componenets/Navbar/Navbar.jsx
+++ src/customer/componenets/Navbar/Navbar.jsx
@@ -455,19 +455,24 @@ export default function Navigation() {
                 </div>
 
                 {/* Cart */}
+                {auth.user && (
                 <div className="ml-4 flow-root lg:ml-6">
                   <a href="#" className="group -m-2 flex items-center p-2">
                     <ShoppingBagIcon
                       aria-hidden="true"
                       className="size-6 shrink-0 text-gray-400 group-hover:text-gray-500"
                     />
+                    
 
-                    <span className="ml-2 text-sm font-medium text-gray-700 group-hover:text-gray-800">
-                    {order.order?.orderItemList ? order.order.orderItemList.length : 0}
-                    </span>
-                    <span className="sr-only">items in cart, view bag</span>
+  <>
+    <span className="ml-2 text-sm font-medium text-gray-700 group-hover:text-gray-800">
+      {order.order?.orderItemList ? order.order.orderItemList.length : 0}
+    </span>
+    <span className="sr-only">items in cart, view bag</span>
+  </>
                   </a>
                 </div>
+)}
               </div>
             </div>
           </div>
